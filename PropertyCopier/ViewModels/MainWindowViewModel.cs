9d4bf0ec039d440a5ba8399ce2699c59a7cc9b5b
Schofield, stuart <546wine@gmail.com>
2023-03-29 17:42:23 -0600

    Add project files.

---
+++ PropertyCopier/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PropertyCopier.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ PropertyCopier/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PropertyCopier.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
