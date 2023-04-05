58ae5f48d626f9210a03299f09ec398335a2500a
Schofield, stuart <546wine@gmail.com>
2023-04-04 18:00:47 -0600

    Deleted the test project

--- PropertyCopier/ViewModels/MainWindowViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace PropertyCopier.ViewModels
-{
-    public class MainWindowViewModel : ObservableObject
-    {
-        public string Greeting => "Welcome to Avalonia!";
-    }
-}
